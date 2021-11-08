62919c1a213a1d651d77225d4bcd9b364302a27f
Iaddiop <ibrahimadiop.idp@gmail.com>
2021-11-09 00:46:35 +0100

    update etl.py

--- etl.py
+++ etl.py
@@ -4,40 +4,43 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
+psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs) # to handle this error i use this post on Stackoverflow : https://stackoverflow.com/questions/50626058/psycopg2-cant-adapt-type-numpy-int64
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['num_songs', 'title', 'artist_id', 'year', 'duration']].loc[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].loc[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = ([t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday])
+    column_labels = (['timestamp', 'hour', 'day', 'week','month', 'year', 'weekday'])
+    time_df = pd.DataFrame({column_labels[i] : time_data[i] for i in range(len(column_labels))})
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['sessionId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +59,16 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (
+        pd.to_datetime(row.ts, unit = 'ms'),
+        row.userId, 
+        row.level, 
+        songid, 
+        artistid, 
+        row.sessionId, 
+        row.location, 
+        row.userAgent
+        )
         cur.execute(songplay_table_insert, songplay_data)
 
 
